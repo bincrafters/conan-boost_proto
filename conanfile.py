1893f01e3bd6ecc3239fddf40144e54d009792d5
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:34:09 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,22 +2,22 @@ from conans import ConanFile, tools, os
 
 class BoostProtoConan(ConanFile):
     name = "Boost.Proto"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-proto"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["proto"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Fusion/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Range/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
-                      "Boost.Typeof/1.64.0@bincrafters/testing",\
-                      "Boost.Utility/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Fusion/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Range/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing",\
+                      "Boost.Typeof/1.65.1@bincrafters/testing",\
+                      "Boost.Utility/1.65.1@bincrafters/testing"
 
                       #config0 core2 fusion5 mpl5 preprocessor0 range7 static_assert1 type_traits3 typeof5 utility5
 
--- upload.sh
+++ upload.sh
@@ -1,1 +1,1 @@
-conan upload --all --remote bincrafters Boost.Proto/1.64.0@bincrafters/testing
+conan upload --all --remote bincrafters Boost.Proto/1.65.1@bincrafters/testing
